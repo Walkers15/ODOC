a4f4027de40c422d159e731d67fa61eabe9e8a64
백승훈 <46189249+Walkers15@users.noreply.github.com>
2019-12-31 22:21:12 +0900

    Create dec3119x.java

---
+++ dec3119x.java
@@ -0,0 +1,52 @@
+//
+import java.util.*;
+
+public class dec3119 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int vc = sc.nextInt(); //Village Count
+        int Capacity = sc.nextInt();
+        int bc = sc.nextInt();//boxCount
+        ArrayList<box> arr = new ArrayList<>();
+        for(int i = 0 ; i < bc ; i++){
+            arr.add(new box(sc.nextInt(), sc.nextInt(), sc.nextInt()));
+        }
+        Collections.sort(arr);
+        int tc = 0;//temp Capacity
+        for(int i = 0 ; i < vc ; i++){
+            while(tc != Capacity){
+                for(int j = 0 ; j < bc ; j++){
+                    if(arr.get(j).from == i){
+                        tc += arr.get(j).count;
+                        if(tc > Capacity)
+                            tc = Capacity;
+                    }
+                }
+            }
+        }
+        System.out.println(arr);
+    }
+}
+class box implements Comparable<box>{
+    int from;
+    int to;
+    int count;
+    box(int from, int to, int count){
+        this.from = from;
+        this.to = to;
+        this.count = count;
+    }
+    @Override
+    public int compareTo(box B){
+        if(this.to > B.to) return 1;
+        else if(this.to == B.to){
+            if(this.count > B.count) return -1;
+            else if(this.count < B.count) return 1;
+            return 0;
+        }
+        else return -1;
+    }
+    public String toString(){
+        return (this.from + " " + this.to + " " + this.count);
+    }
+}
