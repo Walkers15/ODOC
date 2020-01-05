b9c0217edeafe04eb8ea4636c5b060939cd53ca8
백승훈 <46189249+Walkers15@users.noreply.github.com>
2020-01-05 23:22:22 +0900

    Create jan0520_sundayisholiday.kt

---
+++ jan0520_sundayisholiday.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("Sunday is Holiday!")
+    println("Let's Dance!")
+    println("\\O\\")
+    Thread.sleep(1000)
+    println("/O/")
+    Thread.sleep(1000)
+    println("\\O\\")
+    Thread.sleep(1000)
+    println("/O/")
+    Thread.sleep(1000)
+}
