92a124255d5078a9f826a0d0a497de4c0f829064
백승훈 <46189249+Walkers15@users.noreply.github.com>
2020-01-08 17:39:40 +0900

    Create jan0820_minHeap_usePQ.java

---
+++ jan0820_minHeap_usePQ.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class jan0820_1 {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int loop = sc.nextInt();
+        int n;
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for(int i = 0 ; i < loop ; i++){
+            n = sc.nextInt();
+            if( n == 0 ) {
+                if (heap.isEmpty()) System.out.println("0");
+                else System.out.println(heap.poll());
+            }
+            else heap.add(n);
+        }
+    }
+}
