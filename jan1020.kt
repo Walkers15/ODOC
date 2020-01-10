098c85347df2a044781720b2d8c87d4d78607429
백승훈 <46189249+Walkers15@users.noreply.github.com>
2020-01-10 19:48:28 +0900

    Create jan1020.kt

---
+++ jan1020.kt
@@ -0,0 +1,11 @@
+//귀여운 고양이
+//https://www.acmicpc.net/problem/10171
+fun main(){
+    val cat = """
+        \    /\
+         )  ( ')
+        (  /  )
+         \(__)|
+    """.trimIndent()
+    print(cat)
+}
