458ccc0c71322f023158876a8d2f117e7f0f9e13
백승훈 <46189249+Walkers15@users.noreply.github.com>
2020-01-16 19:58:56 +0900

    Create jan1620_Fibo.java

---
+++ jan1620_Fibo.java
@@ -0,0 +1,16 @@
+//https://www.acmicpc.net/problem/2748
+//90번째까지 피보나치로 가면 수가 엄청 커지니까 long형 사용해야 함!
+import java.util.*;
+public class jan1620 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long [] arr = new long[91];
+        arr[0] = 0;
+        arr[1] = 1;
+        for(int i = 2 ; i <= n ; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        System.out.println(arr[n]);
+    }
+}
