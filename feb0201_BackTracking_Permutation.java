8587738fc0c9ab7e125e2979fdc6a2f230e9518c
백승훈 <46189249+Walkers15@users.noreply.github.com>
2020-02-01 15:57:25 +0900

    Create feb0201_BackTracking_Permutation.java

---
+++ feb0201_BackTracking_Permutation.java
@@ -0,0 +1,32 @@
+//https://www.acmicpc.net/problem/15649
+//백트래킹 알고리즘 - DFS , 작동 원리 이해할 
+package boj.java;
+
+import java.util.Scanner;
+
+public class feb0120 {
+    static int n,m;
+    static boolean[] visit = new boolean[9];
+    static int[] arr = new int[9];
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt(); m = sc.nextInt();
+        dfs(0);
+    }
+    public static void dfs(int num) {
+        if(num == m){
+            for(int i = 0 ; i < m ; i++)
+                System.out.print(arr[i] + " ");
+            System.out.println();
+        } else {
+            for(int i = 1 ; i <= n ; i++){
+                if(visit[i] == false){
+                    arr[num] = i;
+                    visit[i] = true;
+                    dfs(num+1);
+                    visit[i] = false;
+                }
+            }
+        }
+    }
+}
