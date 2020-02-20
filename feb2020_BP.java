c759d52b686826f9b0eca0e0aa76f52999bf2acf
백승훈 <46189249+Walkers15@users.noreply.github.com>
2020-02-20 23:43:44 +0900

    브루트포스_블랙잭

---
+++ feb2020_BP.java
@@ -0,0 +1,31 @@
+//https://www.acmicpc.net/problem/2798
+//전형적인 브루트-포스 문제, DFS로 해결하는 방법도 있을 듯?
+package boj.java;
+
+import java.util.Scanner;
+
+public class feb2020 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int min = Integer.MAX_VALUE;
+        int cur = 0;
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i++) arr[i] = sc.nextInt();
+        for(int i = 0 ; i < n ; i++){
+           for(int j = i+1 ; j < n ; j++){
+               for(int k = j+1 ; k < n ; k++){
+                   cur = arr[i]+arr[j]+arr[k];
+                   if(cur <= m && m-cur < min){
+                       if(m-cur == 0){
+                           System.out.println(m);
+                           System.exit(0);
+                       } else min = m-cur;
+                   }
+               }
+           }
+        }
+        System.out.println(m-min);
+    }
+}
