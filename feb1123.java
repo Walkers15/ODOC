e621b996626f8609826ad75c4d6c98b639102b60
Walkers15 <qortmd7777@gmail.com>
2023-02-11 23:54:29 +0900

    add: 18108

---
+++ feb1123.java
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/18108
+
+import java.util.Scanner;
+
+public class feb1123 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    System.out.println(n - 543);
+    sc.close();
+  }
+}
