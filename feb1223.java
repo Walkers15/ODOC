1b5280c18cbb873cf9a98a816689d8e4667b2d39
Walkers15 <qortmd7777@gmail.com>
2023-02-12 14:01:25 +0900

    add: 3003

---
+++ feb1223.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class feb1223 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int k = 1 - sc.nextInt();
+    int q = 1 - sc.nextInt();
+    int r = 2 - sc.nextInt();
+    int b = 2 - sc.nextInt();
+    int n = 2 - sc.nextInt();
+    int p = 8 - sc.nextInt();
+
+    System.out.printf("%d %d %d %d %d %d\n", k, q, r, b, n, p);
+  }
+}
