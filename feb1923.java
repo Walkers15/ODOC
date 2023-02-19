f188244e9062511a0230f9dd44c67bf4de4a4ee6
Walkers15 <qortmd7777@gmail.com>
2023-02-19 19:58:19 +0900

    add: 1271 엄청난 부자2

---
+++ feb1923.java
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/1271
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class feb1923 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    BigInteger n = sc.nextBigInteger();
+    BigInteger m = sc.nextBigInteger();
+    sc.close();
+    System.out.println(n.divide(m));
+    System.out.println(n.remainder(m));
+  }
+}
