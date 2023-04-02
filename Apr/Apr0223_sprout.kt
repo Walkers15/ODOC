d60a4e46c102b96416bab82dfa1d8d9043bf6da4
Walkers15 <qortmd7777@gmail.com>
2023-04-02 22:20:43 +0900

    add: 25083 새싹

---
+++ Apr/Apr0223_sprout.kt
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/25083 새싹
+
+fun main(){
+    val sprout = """
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+    """.trimIndent()
+    print(sprout)
+}
